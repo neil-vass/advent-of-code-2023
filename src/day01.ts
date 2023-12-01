2e86f7f9548163975ec52e4e815006758eefddfd
Neil Vass <neil.vass+github@gmail.com>
2023-12-01 13:01:42 +0000

    Day 1 complete

--- src/day01.ts
+++ src/day01.ts
@@ -10,13 +10,44 @@ export function calibrationValue(s: string) {
     return Number(digits.at(0) + digits.at(-1));
 }
 
-export async function sumCalibrationValues(input: Sequence<string>) {
-    const values = input.map(s => calibrationValue(s));
+export function calibrationValueWithLetters(s: string) {
+    const lettersToDigits = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+
+    const wordsAndDigits: string[] = [];
+    const r = /(?=(one|two|three|four|five|six|seven|eight|nine|\d))./g
+    let m = r.exec(s);
+    while(m !== null) {
+        wordsAndDigits.push(m[1]);
+        m = r.exec(s);
+    }
+
+    if (wordsAndDigits.length === 0) throw new Error(`Line has no numbers: ${s}`);
+
+    // @ts-ignore From above check, we know digits has at least one element.
+    const first = lettersToDigits[wordsAndDigits.at(0)] || wordsAndDigits.at(0);
+    // @ts-ignore From above check, we know digits has at least one element.
+    const last = lettersToDigits[wordsAndDigits.at(-1)] || wordsAndDigits.at(-1);
+
+    return Number(first + last);
+}
+
+export async function sumCalibrationValues(input: Sequence<string>, valueFn=calibrationValue) {
+    const values = input.map(s => valueFn(s));
     return Sequence.sum(values);
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const input = linesFromFile("./data/day01.txt");
-    console.log(await sumCalibrationValues(input));
+    console.log(await sumCalibrationValues(input, calibrationValueWithLetters));
 }
--- test/day01.test.ts
+++ test/day01.test.ts
@@ -19,3 +19,29 @@ describe("Part 1", () => {
         expect(await day01.sumCalibrationValues(input)).toBe(142);
     });
 });
+
+describe("Part 2", () => {
+    it("Finds calibration value from string", () => {
+        expect(day01.calibrationValueWithLetters("two1nine")).toBe(29);
+        expect(day01.calibrationValueWithLetters("eightwothree")).toBe(83);
+        expect(day01.calibrationValueWithLetters("abcone2threexyz")).toBe(13);
+        expect(day01.calibrationValueWithLetters("xtwone3four")).toBe(24);
+        expect(day01.calibrationValueWithLetters("4nineeightseven2")).toBe(42);
+        expect(day01.calibrationValueWithLetters("zoneight234")).toBe(14);
+        expect(day01.calibrationValueWithLetters("7pqrstsixteen")).toBe(76);
+    });
+
+    it("Sums calibration values from sequence of strings", async () => {
+        const input = new Sequence([
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"
+        ]);
+
+        expect(await day01.sumCalibrationValues(input, day01.calibrationValueWithLetters)).toBe(281);
+    });
+});
