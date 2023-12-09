527e98203677c01bbcb0241c0666dcae327f7f1e
Neil Vass <neil.vass+github@gmail.com>
2023-12-09 22:16:00 +0000

    Setup day 10

---
+++ test/day10.test.ts
@@ -0,0 +1,8 @@
+import {expect, describe, it} from "vitest";
+import * as day10 from "../src/day10.js";
+
+describe("#fn", () => {
+    it("runs", () => {
+        expect(day10.fn("")).toBe("a real test");
+    });
+});
