08253e622166704017c2d630668ca1886f833697
Neil Vass <neil.vass+github@gmail.com>
2023-12-10 15:21:42 +0000

    Day 10 done

--- src/day10.ts
+++ src/day10.ts
@@ -1,12 +1,210 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
 
-export function fn(filepath: string) {
-    return "Hello, World!";
+type Pos = { x: number, y: number };
+type ConnectionsList = Array<Pos>;
+type GridRow = Array<ConnectionsList>;
+type Grid = Array<GridRow>;
+
+export class PipeMap {
+
+    private constructor(readonly grid: Grid,
+                        readonly start: Pos,
+                        readonly originalChars: Array<string>) {
+        this.removeBlockedConnections();
+    }
+
+    private removeBlockedConnections() {
+        // This is O(scary), but we only run it once.
+        for (let rowNum=1; rowNum < this.grid.length-1; rowNum++) {
+            for (let colNum=1; colNum < this.grid[0].length-1; colNum++) {
+                const currentConnections = this.grid[rowNum][colNum];
+                const unblockedConnections = new Array<Pos>();
+                for (const n of currentConnections) {
+                    const neighboursConnections = this.grid[n.x][n.y];
+                    for (const nc of neighboursConnections) {
+                        if (nc.x === rowNum && nc.y === colNum) {
+                            unblockedConnections.push(n);
+                        }
+                    }
+                }
+                this.grid[rowNum][colNum] = unblockedConnections;
+            }
+        }
+    }
+
+    neighbours(tile: Pos) {
+        return this.grid[tile.x][tile.y];
+    }
+
+    tilesInLoop() {
+        let current = this.start;
+        let loopSoFar = new Array<Pos>();
+        let stepCount = 0;
+        while (true) {
+            let options = this.grid[current.x][current.y];
+            if (loopSoFar.length > 0) {
+                const cameFrom = loopSoFar.at(-1)!;
+                options = options.filter(n => !(n.x === cameFrom.x && n.y === cameFrom.y));
+            }
+            const next = options[0];
+            loopSoFar.push(current);
+            current = next;
+            stepCount++;
+            if (current.x === this.start.x && current.y === this.start.y) break;
+        }
+        return loopSoFar;
+    }
+
+    stepsToFarthestPoint() {
+        const loop = this.tilesInLoop();
+        return loop.length / 2;
+    }
+
+
+    private lookRight(lookingFrom: Pos, direction: Pos, loop: Pos[], seen: Set<string>) {
+        // We'll neaten this, don't worry.
+        if (direction.x < 0) {
+
+            const neighboursLookingRight = loop.filter(t => t.x === lookingFrom.x && t.y > lookingFrom.y).map(t => t.y);
+            const firstNeighbourY = Math.min(...neighboursLookingRight);
+            const x = lookingFrom.x;
+            for (let y = lookingFrom.y+1; y < firstNeighbourY; y++) {
+                seen.add(JSON.stringify({x, y}));
+            }
+
+        } else if (direction.y > 0) {
+
+            const neighboursLookingRight = loop.filter(t => t.x > lookingFrom.x && t.y === lookingFrom.y).map(t => t.x);
+            const firstNeighbourX = Math.min(...neighboursLookingRight);
+            const y = lookingFrom.y;
+            for (let x = lookingFrom.x+1; x < firstNeighbourX; x++) {
+                seen.add(JSON.stringify({x, y}));
+            }
+
+        } else if (direction.x > 0) {
+
+            const neighboursLookingRight = loop.filter(t => t.x === lookingFrom.x && t.y < lookingFrom.y).map(t => t.y);
+            const firstNeighbourY = Math.max(...neighboursLookingRight);
+            const x = lookingFrom.x;
+            for (let y = lookingFrom.y-1; y > firstNeighbourY; y--) {
+                seen.add(JSON.stringify({x, y}));
+            }
+
+
+        } else if (direction.y < 0) {
+
+            const neighboursLookingRight = loop.filter(t => t.x < lookingFrom.x && t.y === lookingFrom.y).map(t => t.x);
+            const firstNeighbourX = Math.max(...neighboursLookingRight);
+            const y = lookingFrom.y;
+            for (let x = lookingFrom.x-1; x > firstNeighbourX; x--) {
+                seen.add(JSON.stringify({x, y}));
+            }
+        }
+    }
+
+    private isClockwise(loop: Array<Pos>) {
+        let sum = 0;
+        for (let i = 0; i < loop.length - 1; i++) {
+            sum += (loop[i + 1].x - loop[i].x) * (loop[i + 1].y + loop[i].y);
+        }
+        return (sum > 0);
+    }
+
+    areaEnclosedByLoop() {
+        const loop = this.tilesInLoop();
+        if (!this.isClockwise(loop)) loop.reverse();
+
+        // Step round the loop, looking right. Say what you see.
+        const enclosed = new Set<string>();
+        for (const [idx, current] of loop.entries()) {
+            const cameFrom = idx === 0 ? loop.at(-1)! : loop[idx-1];
+            const lastDirection = {x: current.x - cameFrom.x, y: current.y - cameFrom.y};
+            this.lookRight(current, lastDirection, loop, enclosed);
+
+            const next = idx === loop.length-1 ? loop[0] : loop[idx+1];
+            const nextDirection = {x: next.x - current.x, y: next.y - current.y};
+            this.lookRight(current, nextDirection, loop, enclosed);
+        }
+        return enclosed.size;
+    }
+
+    drawLoop() {
+        // For debugging.
+        const loop = this.tilesInLoop();
+        for (let x=0; x < this.grid.length; x++) {
+            let str = "";
+            for (let y=0; y < this.grid[0].length; y++) {
+               if (loop.some(t => t.x === x && t.y === y)) {
+                   str += this.originalChars[x][y];
+               } else {
+                   str += ".";
+               }
+            }
+            console.log(str);
+        }
+    }
+
+    static async buildFromDescription(lines: Sequence<string>) {
+        // Main work is to build a grid of connections.
+        // Pad with "." for easier bounds checking.
+        const paddingTile = new Array<Pos>();
+        const grid = new Array<GridRow>();
+        const originalChars = new Array<string>(); // For debugging.
+        let start: Pos | null = null;
+        let rowNum = 0;
+
+        for await (const line of lines) {
+            rowNum++;
+            const row = new Array<ConnectionsList>();
+            let colNum = 0;
+            row.push(paddingTile);
+            for await (const tileType of line) {
+                colNum++;
+                row.push(tileConnections(tileType, rowNum, colNum));
+                if (tileType === "S") start = {x: rowNum, y: colNum};
+            }
+            row.push(paddingTile);
+            grid.push(row);
+            originalChars.push(`.${line}.`);
+        }
+
+        const paddingRow = Array.from(grid[0], () => paddingTile);
+        grid.unshift(paddingRow);
+        grid.push(paddingRow);
+        originalChars.unshift("");
+        originalChars.push("");
+        if (start === null) throw new Error(`Didn't find start tile`);
+        return new PipeMap(grid, start, originalChars);
+    }
+}
+
+export function tileConnections(tileType: string, tileX: number, tileY: number): ConnectionsList {
+    switch (tileType) {
+        case "|": return [{x: tileX-1, y: tileY}, {x: tileX+1, y: tileY}];
+        case "-": return [{x: tileX, y: tileY+1}, {x: tileX, y: tileY-1}];
+        case "L": return [{x: tileX-1, y: tileY}, {x: tileX, y: tileY+1}];
+        case "J": return [{x: tileX-1, y: tileY}, {x: tileX, y: tileY-1}];
+        case "7": return [{x: tileX+1, y: tileY}, {x: tileX, y: tileY-1}];
+        case "F": return [{x: tileX, y: tileY+1}, {x: tileX+1, y: tileY}];
+        case ".": return new Array<Pos>();
+        case "S": return [{x: tileX-1, y: tileY}, {x: tileX, y: tileY+1}, {x: tileX+1, y: tileY}, {x: tileX, y: tileY-1}];
+        default: throw new Error(`Unknown tile type: ${tileType}`);
+    }
+}
+
+export async function solvePart1(lines: Sequence<string>) {
+    const pipeMap = await PipeMap.buildFromDescription(lines);
+    return pipeMap.stepsToFarthestPoint();
+}
+
+export async function solvePart2(lines: Sequence<string>) {
+    const pipeMap = await PipeMap.buildFromDescription(lines);
+    return pipeMap.areaEnclosedByLoop();
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = "./data/day10.txt";
-    console.log(fn(filepath));
+    console.log(await solvePart2(linesFromFile(filepath)));
 }
--- test/day10.test.ts
+++ test/day10.test.ts
@@ -1,8 +1,141 @@
 import {expect, describe, it} from "vitest";
 import * as day10 from "../src/day10.js";
+import {Sequence} from "../src/sequence.js";
 
-describe("#fn", () => {
-    it("runs", () => {
-        expect(day10.fn("")).toBe("a real test");
+
+describe("Part 1", () => {
+    it("Plumbs tile connections", () => {
+        expect(day10.tileConnections("|", 1, 1)).toStrictEqual([{x:0, y:1}, {x:2, y:1}]);
+        expect(day10.tileConnections("-", 1, 1)).toStrictEqual([{x:1, y:2}, {x:1, y:0}]);
+        expect(day10.tileConnections("L", 1, 1)).toStrictEqual([{x:0, y:1}, {x:1, y:2}]);
+        expect(day10.tileConnections("J", 1, 1)).toStrictEqual([{x:0, y:1}, {x:1, y:0}]);
+        expect(day10.tileConnections("7", 1, 1)).toStrictEqual([{x:2, y:1}, {x:1, y:0}]);
+        expect(day10.tileConnections("F", 1, 1)).toStrictEqual([{x:1, y:2}, {x:2, y:1}]);
+        expect(day10.tileConnections(".", 1, 1)).toStrictEqual([]);
+        expect(day10.tileConnections("S", 1, 1)).toStrictEqual([{x:0, y:1}, {x:1, y:2}, {x:2, y:1}, {x:1, y:0}]);
+    });
+
+    it("Builds grid from description", async () => {
+        const lines = new Sequence([
+            ".....",
+            ".S-7.",
+            ".|.|.",
+            ".L-J.",
+            ".....",
+        ]);
+
+        const pipeMap = await day10.PipeMap.buildFromDescription(lines);
+        // Implementation detail: we pad the map, so top left here is (1,1).
+        expect(pipeMap.start).toStrictEqual({x:2, y:2});
+
+        // From "S" we can go right or down.
+        expect(pipeMap.neighbours({x:2, y:2})).toStrictEqual([{x:2, y:3}, {x:3, y:2}]);
+        // From "L" we can go up or right.
+        expect(pipeMap.neighbours({x:4, y:2})).toStrictEqual([{x:3, y:2}, {x:4, y:3}]);
+    });
+
+    it("Solves tiny example", async () => {
+        const lines = new Sequence([
+            ".....",
+            ".S-7.",
+            ".|.|.",
+            ".L-J.",
+            ".....",
+        ]);
+
+        expect(await day10.solvePart1(lines)).toBe(4);
+    });
+
+    it("Solves more complex example", async () => {
+        const lines = new Sequence([
+            "7-F7-",
+            ".FJ|7",
+            "SJLL7",
+            "|F--J",
+            "LJ.LJ",
+        ]);
+
+        expect(await day10.solvePart1(lines)).toBe(8);
+    });
+
+});
+
+describe("Part 2", () => {
+    it("Solves trivial case ", async () => {
+        const lines = new Sequence([
+            ".....",
+            ".S-7.",
+            ".|.|.",
+            ".L-J.",
+            ".....",
+        ]);
+
+        expect(await day10.solvePart2(lines)).toBe(1);
+    });
+
+    it("Solves first example", async () => {
+        const lines = new Sequence([
+            "...........",
+            ".S-------7.",
+            ".|F-----7|.",
+            ".||.....||.",
+            ".||.....||.",
+            ".|L-7.F-J|.",
+            ".|..|.|..|.",
+            ".L--J.L--J.",
+            "...........",
+        ]);
+
+        expect(await day10.solvePart2(lines)).toBe(4);
+    });
+
+    it("Solves squeezed example", async () => {
+        const lines = new Sequence([
+            "..........",
+            ".S------7.",
+            ".|F----7|.",
+            ".||....||.",
+            ".||....||.",
+            ".|L-7F-J|.",
+            ".|..||..|.",
+            ".L--JL--J.",
+            "..........",
+        ]);
+
+        expect(await day10.solvePart2(lines)).toBe(4);
+    });
+
+    it("Solves larger example", async () => {
+        const lines = new Sequence([
+            ".F----7F7F7F7F-7....",
+            ".|F--7||||||||FJ....",
+            ".||.FJ||||||||L7....",
+            "FJL7L7LJLJ||LJ.L-7..",
+            "L--J.L7...LJS7F-7L7.",
+            "....F-J..F7FJ|L7L7L7",
+            "....L7.F7||L7|.L7L7|",
+            ".....|FJLJ|FJ|F7|.LJ",
+            "....FJL-7.||.||||...",
+            "....L---J.LJ.LJLJ...",
+        ]);
+
+        expect(await day10.solvePart2(lines)).toBe(8);
+    });
+
+    it("Solves last example", async () => {
+        const lines = new Sequence([
+            "FF7FSF7F7F7F7F7F---7",
+            "L|LJ||||||||||||F--J",
+            "FL-7LJLJ||||||LJL-77",
+            "F--JF--7||LJLJ7F7FJ-",
+            "L---JF-JLJ.||-FJLJJ7",
+            "|F|F-JF---7F7-L7L|7|",
+            "|FFJF7L7F-JF7|JL---7",
+            "7-L-JL7||F7|L7F-7F7|",
+            "L.L7LFJ|||||FJL7||LJ",
+            "L7JLJL-JLJLJL--JLJ.L",
+        ]);
+
+        expect(await day10.solvePart2(lines)).toBe(10);
     });
 });
