fd25b347e7dc3ba2819234a2a4e213cfe1b30a83
Neil Vass <neil.vass+github@gmail.com>
2023-12-11 11:47:36 +0000

    Day 11 done

--- src/day11.ts
+++ src/day11.ts
@@ -1,12 +1,93 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
 
-export function fn(filepath: string){
-    return "Hello, World!";
+type Pos = { x: number, y: number };
+
+export function numEntriesSmallerThan(n: number, orderedArray: Array<number>) {
+    for (let i=0; i < orderedArray.length; i++) {
+        if (orderedArray[i] >= n) {
+            return i;
+        }
+    }
+    return orderedArray.length;
+}
+
+export class StarMap {
+    private constructor(readonly galaxies: Map<number, Pos>,
+                        readonly emptyRows: Set<number>,
+                        readonly emptyCols: Set<number>,
+                        readonly expansionRate: number) {
+        this.expand(expansionRate);
+    }
+
+    private expand(rate: number) {
+        const emptyRowsAscending = [...this.emptyRows].sort((a,b) => a-b);
+        const emptyColsAscending = [...this.emptyCols].sort((a,b) => a-b);
+
+        for (const g of this.galaxies.values()) {
+            g.x += numEntriesSmallerThan(g.x, emptyRowsAscending) * (rate-1);
+            g.y += numEntriesSmallerThan(g.y, emptyColsAscending) * (rate-1);
+        }
+    }
+
+    shortestPathLength(fromGalaxy: number, toGalaxy: number) {
+        const f: Pos = this.galaxies.get(fromGalaxy)!;
+        const t: Pos = this.galaxies.get(toGalaxy)!;
+        return Math.abs(f.x - t.x) + Math.abs(f.y - t.y);
+    }
+
+    sumOfShortestPaths() {
+        const galaxyCount = this.galaxies.size;
+        let sum = 0;
+        for (let from = 1; from < galaxyCount; from++) {
+            for (let to = from+1; to <= galaxyCount; to++) {
+                sum += this.shortestPathLength(from, to);
+            }
+        }
+        return sum;
+    }
+
+    static async buildFromDescription(lines: Sequence<string>, expansionRate=2) {
+        const galaxies = new Map<number, Pos>();
+        const emptyRows = new Set<number>();
+        let emptyCols: Set<number> | null = null;
+
+        let galaxyCount = 0;
+        let x = 0;
+
+        for await (const line of lines) {
+            if (emptyCols === null) {
+                emptyCols = new Set(Array.from(line, (val, idx) => idx));
+            }
+            emptyRows.add(x);
+            for (let y = 0; y < line.length; y++) {
+                if (line[y] === "#") {
+                    galaxyCount++;
+                    galaxies.set(galaxyCount, {x,y});
+                    emptyRows.delete(x);
+                    emptyCols.delete(y);
+                }
+            }
+            x++;
+        }
+
+        if (emptyCols === null) throw new Error(`Empty map!`);
+        return new StarMap(galaxies, emptyRows, emptyCols, expansionRate);
+    }
+}
+
+export async function solvePart1(lines: Sequence<string>) {
+    const starMap = await StarMap.buildFromDescription(lines);
+    return starMap.sumOfShortestPaths();
+}
+
+export async function solvePart2(lines: Sequence<string>, expansionRate: number) {
+    const starMap = await StarMap.buildFromDescription(lines, expansionRate);
+    return starMap.sumOfShortestPaths();
 }
 
 // If this script was invoked directly on the command line:
-if(`file://${process.argv[1]}` === import.meta.url) {
+if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = "./data/day11.txt";
-    console.log(fn(filepath));
+    console.log(await solvePart2(linesFromFile(filepath), 1_000_000));
 }
--- test/day11.test.ts
+++ test/day11.test.ts
@@ -1,8 +1,59 @@
-import {expect, describe, it} from "vitest";
+import {expect, describe, it, beforeEach} from "vitest";
 import * as day11 from "../src/day11.js";
+import {Sequence} from "../src/sequence.js";
 
-describe("#fn", () => {
-    it("runs", () => {
-        expect(day11.fn("")).toBe("a real test");
+// Same map from the example for each test.
+let lines: Sequence<string>;
+beforeEach(() => {
+    lines = new Sequence([
+        "...#......",
+        ".......#..",
+        "#.........",
+        "..........",
+        "......#...",
+        ".#........",
+        ".........#",
+        "..........",
+        ".......#..",
+        "#...#.....",
+    ]);
+});
+
+describe("Part 1", () => {
+    it("Counts numEntriesSmallerThan", () => {
+        expect(day11.numEntriesSmallerThan(10, [1, 3, 4, 10, 12])).toBe(3);
+        expect(day11.numEntriesSmallerThan(8, [0,1,2])).toBe(3);
+        expect(day11.numEntriesSmallerThan(3, [5,6])).toBe(0);
+    });
+
+    it("Finds galaxy positions", async () => {
+        const starMap = await day11.StarMap.buildFromDescription(lines);
+        expect(starMap.galaxies.size).toBe(9);
+
+        // Map has expanded
+        expect(starMap.galaxies.get(1)).toStrictEqual({x:0, y:4});
+        expect(starMap.galaxies.get(2)).toStrictEqual({x:1, y:9});
+
+        expect(starMap.emptyRows).toStrictEqual(new Set([3, 7]));
+        expect(starMap.emptyCols).toStrictEqual(new Set([2, 5, 8]));
+    });
+
+    it("Finds shortest paths", async () => {
+        const starMap = await day11.StarMap.buildFromDescription(lines);
+        expect(starMap.shortestPathLength(5, 9)).toBe(9);
+        expect(starMap.shortestPathLength(1, 7)).toBe(15);
+        expect(starMap.shortestPathLength(3, 6)).toBe(17);
+        expect(starMap.shortestPathLength(8, 9)).toBe(5);
+    });
+
+    it("Solves example", async () => {
+        expect(await day11.solvePart1(lines)).toBe(374);
+    });
+});
+
+
+describe("Part 2", () => {
+    it("Solves example", async () => {
+        expect(await day11.solvePart2(lines, 10)).toBe(1030);
     });
 });
