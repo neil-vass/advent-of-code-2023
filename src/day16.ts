e3c9252dd6c07f1d8ee016ad93b6f05c727ebfb5
Neil Vass <neil.vass+github@gmail.com>
2023-12-17 23:18:16 +0000

    Day 16 part 1 done

--- src/day16.ts
+++ src/day16.ts
@@ -1,31 +1,111 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
+import {FifoQueue, Graph} from "./graphSearch.js";
 
-export type Dir = { row: -1|0|1, col: -1|0|1 };
-export const North: Dir = { row: -1, col: 0 };
-export const West: Dir = { row: 0, col: -1 };
-export const South: Dir = { row: +1, col: 0 };
-export const East: Dir = { row: 0, col: +1 };
+type Pos = { x: number, y: number };
+export enum Dir { Up, Right, Down, Left};
 
-export class Contraption {
-
-    private grid = new Array<>
-    private constructor() {}
-    static buildFromDescription(lines: Sequence<string>) {
-        return new Contraption();
+export function exitDirections(entryDirection: Dir, tile: string): Dir[] {
+    switch (tile) {
+        case ".":
+            return [entryDirection];
+        case "\\":
+            return [(3-entryDirection) % 4];
+        case "/":
+            return [(5-entryDirection) % 4];
+        case "|":
+            if ([Dir.Up, Dir.Down].includes(entryDirection)) return [entryDirection];
+            else return [Dir.Up, Dir.Down];
+        case "-":
+            if ([Dir.Left, Dir.Right].includes(entryDirection)) return [entryDirection];
+            else return [Dir.Left, Dir.Right];
+        default:
+            throw new Error(`Unknown tile type: ${tile}`);
     }
 }
-export function fn(filepath: string) {
-    return "Hello, World!";
+
+export class HeadOfPath {
+    constructor(readonly pos: Pos,
+                readonly dir: Dir) {}
+
+    neighbourPosition(dir: Dir) {
+        switch(dir) {
+            case Dir.Up: return { x: this.pos.x-1, y: this.pos.y };
+            case Dir.Right: return { x: this.pos.x, y: this.pos.y+1 };
+            case Dir.Down: return { x: this.pos.x+1, y: this.pos.y };
+            case Dir.Left: return { x: this.pos.x, y: this.pos.y-1 };
+        }
+    }
+}
+
+export class Contraption {
+    private constructor(readonly grid: string[][]) {}
+
+    isInsideBounds(pos: Pos) {
+        return (pos.x >= 0 && pos.x < this.grid.length &&
+                pos.y >= 0 && pos.y < this.grid[0].length);
+    }
+
+    neighbours(headOfPath: HeadOfPath): HeadOfPath[] {
+        const tile = this.grid[headOfPath.pos.x][headOfPath.pos.y];
+        const directions = exitDirections(headOfPath.dir, tile);
+
+        const results = new Array<HeadOfPath>();
+        for (const dir of directions) {
+            const pos = headOfPath.neighbourPosition(dir);
+            if (this.isInsideBounds(pos)) results.push(new HeadOfPath(pos, dir));
+        }
+        return results;
+    }
+
+    // Adapted from breadthFirstSearch in ./src/graphSearch.ts.
+    // Returns the collection of tiles energized by beam.
+    propagateBeam(start: Pos, dir: Dir) {
+        const hash = (o: any) => JSON.stringify(o);
+        const startOfPath = new HeadOfPath(start, dir);
+
+        const frontier = new FifoQueue<HeadOfPath>();
+        frontier.push(startOfPath);
+
+        const reachedTiles = new Set<string>();
+        reachedTiles.add(hash(start));
+        const reachedTilesWithDirection = new Set<string>();
+        reachedTilesWithDirection.add(hash(startOfPath));
+
+        while (!frontier.isEmpty()) {
+            const current = frontier.pull()!;
+            for (const n of this.neighbours(current)) {
+                if (!reachedTilesWithDirection.has(hash(n))) {
+                    frontier.push(n);
+                    reachedTiles.add(hash(n.pos));
+                    reachedTilesWithDirection.add(hash(n));
+                }
+            }
+        }
+
+        return reachedTiles;
+    }
+
+    countEnergizedTiles(beamStart: Pos, beamDirection: Dir) {
+        const tiles = this.propagateBeam(beamStart, beamDirection);
+        return tiles.size;
+    }
+
+    static async buildFromDescription(lines: Sequence<string>) {
+        const grid = await lines.map(ln => ln.split("")).toArray();
+        return new Contraption(grid);
+    }
+}
+
+export async function solvePart1(lines: Sequence<string>) {
+    const contraption = await Contraption.buildFromDescription(lines);
+    const beamStart = {x:0, y:0};
+    return contraption.countEnergizedTiles(beamStart, Dir.Right);
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = "./data/day16.txt";
     const lines = linesFromFile(filepath);
-    for await (const line of lines) {
-        console.log(line.length);
-        console.log(line);
-        break;
-    }
+    console.log(await solvePart1(lines));
 }
--- test/day16.test.ts
+++ test/day16.test.ts
@@ -1,11 +1,49 @@
 import {expect, describe, it} from "vitest";
 import * as day16 from "../src/day16.js";
+import {Dir} from "../src/day16.js";
 import {Sequence} from "../src/sequence.js";
 import {linesFromFile} from "../src/helpers.js";
 
 describe("Part 1", () => {
-    it("Builds contraption", () => {
-        const lines = linesFromFile("./test/day16-example.txt")
-        const contraption = day16.Contraption.buildFromDescription(lines);
+    it("Finds exit directions for empty tiles", async () => {
+        expect(day16.exitDirections(Dir.Up, ".")).toStrictEqual([Dir.Up]);
+        expect(day16.exitDirections(Dir.Right, ".")).toStrictEqual([Dir.Right]);
+        expect(day16.exitDirections(Dir.Down, ".")).toStrictEqual([Dir.Down]);
+        expect(day16.exitDirections(Dir.Left, ".")).toStrictEqual([Dir.Left]);
+    });
+
+    it("Finds reflection directions for backslash mirrors", async () => {
+        expect(day16.exitDirections(Dir.Up, "\\")).toStrictEqual([Dir.Left]);
+        expect(day16.exitDirections(Dir.Right, "\\")).toStrictEqual([Dir.Down]);
+        expect(day16.exitDirections(Dir.Down, "\\")).toStrictEqual([Dir.Right]);
+        expect(day16.exitDirections(Dir.Left, "\\")).toStrictEqual([Dir.Up]);
+    });
+
+    it("Finds reflection directions for forward slash mirrors", async () => {
+        expect(day16.exitDirections(Dir.Up, "/")).toStrictEqual([Dir.Right]);
+        expect(day16.exitDirections(Dir.Right, "/")).toStrictEqual([Dir.Up]);
+        expect(day16.exitDirections(Dir.Down, "/")).toStrictEqual([Dir.Left]);
+        expect(day16.exitDirections(Dir.Left, "/")).toStrictEqual([Dir.Down]);
+    });
+
+    it("Finds reflection directions for vertical splitters", async () => {
+        expect(day16.exitDirections(Dir.Up, "|")).toStrictEqual([Dir.Up]);
+        expect(day16.exitDirections(Dir.Right, "|")).toStrictEqual([Dir.Up, Dir.Down]);
+        expect(day16.exitDirections(Dir.Down, "|")).toStrictEqual([Dir.Down]);
+        expect(day16.exitDirections(Dir.Left, "|")).toStrictEqual([Dir.Up, Dir.Down]);
+    });
+
+    it("Finds reflection directions for horizontal splitters", async () => {
+        expect(day16.exitDirections(Dir.Up, "-")).toStrictEqual([Dir.Left, Dir.Right]);
+        expect(day16.exitDirections(Dir.Right, "-")).toStrictEqual([Dir.Right]);
+        expect(day16.exitDirections(Dir.Down, "-")).toStrictEqual([Dir.Left, Dir.Right]);
+        expect(day16.exitDirections(Dir.Left, "-")).toStrictEqual([Dir.Left]);
+    });
+
+    it("Builds contraption", async () => {
+        const lines = linesFromFile("./test/data/day16-example.txt")
+        const contraption = await day16.Contraption.buildFromDescription(lines);
+        const beamStart = {x:0, y:0};
+        expect(contraption.countEnergizedTiles(beamStart, Dir.Right)).toBe(46);
     });
 });
