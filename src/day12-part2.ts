a652d533c927b8443e59499f8d4a996d35e9251d
Neil Vass <neil.vass+github@gmail.com>
2023-12-18 22:47:09 +0000

    Running in good time, solves example, wrong answer on real input.

--- src/day12-part2.ts
+++ src/day12-part2.ts
@@ -1,11 +1,8 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
-import {expect} from "vitest";
-
 
 type ConditionRecord = string;
 type Groups = Array<number>;
-
 
 export function parseLine(line: string): [ConditionRecord, Groups] {
     const [recordStr, groupsStr] = line.split(" ");
@@ -21,51 +18,69 @@ export function unfold(s: string, scale=5) {
     return `${condition} ${groups}`;
 }
 
+let cache = new Map<string, number>();
+let calls = 0;
+let cacheHits = 0;
+
 export function possibleArrangements(condition: ConditionRecord, damaged: Groups): number {
-    // No first group? We're done.
-    if (damaged.length === 0) {
-        if (condition.indexOf("#") === -1) return 1;
-        else return 0;
+    calls++;
+    const hashKey = condition + damaged.toString();
+    const cachedResult = cache.get(hashKey)
+    if (cachedResult !== undefined) {
+        cacheHits++;
+        return cachedResult;
     }
 
-    // Get first group, solve for that, recurse with the rest of the info.
-    const [sizeOfFirstGroup, ...restOfGroups] = damaged;
-    const firstQ = condition.indexOf("?");
-    const thereIsAQ = firstQ !== -1;
-    const firstHash = condition.indexOf("#");
-    const thereIsAHash = firstHash !== -1;
-
-    if (thereIsAQ && (!thereIsAHash || firstQ < firstHash)) {
-        const biggestGroupThisCouldMake = condition.slice(firstQ).match(/^[?#]+/)![0].length;
-        if (biggestGroupThisCouldMake < sizeOfFirstGroup) {
-            // Must just be a dot. String up to and including it can be dropped.
-            return possibleArrangements(condition.slice(firstQ+1), damaged);
-        } else {
-            // Worth exploring.
-            const dot = condition.slice(firstQ+1);
-            const hash = "#" + dot;
-            return possibleArrangements(dot, damaged) + possibleArrangements(hash, damaged);
+    function wrapped() {
+        // No first group? We're done.
+        if (damaged.length === 0) {
+            if (condition.indexOf("#") === -1) return 1;
+            else return 0;
         }
-    }
 
-    if (firstHash !== -1) {
-        const groupEndIdx = firstHash+sizeOfFirstGroup;
-        const groupChunk = condition.slice(firstHash, groupEndIdx);
-        const isWholeGroup = (groupEndIdx === condition.length) || (condition[groupEndIdx] !== "#");
-        const canMakeGroup = groupChunk.match(/^[?#]+$/) && isWholeGroup;
+        // Get first group, solve for that, recurse with the rest of the info.
+        const [sizeOfFirstGroup, ...restOfGroups] = damaged;
+        const firstQ = condition.indexOf("?");
+        const thereIsAQ = firstQ !== -1;
+        const firstHash = condition.indexOf("#");
+        const thereIsAHash = firstHash !== -1;
 
-        if (canMakeGroup) {
-            return possibleArrangements(condition.slice(groupEndIdx+1), restOfGroups);
+        if (thereIsAQ && (!thereIsAHash || firstQ < firstHash)) {
+            const biggestGroupThisCouldMake = condition.slice(firstQ).match(/^[?#]+/)![0].length;
+            if (biggestGroupThisCouldMake < sizeOfFirstGroup) {
+                // Must just be a dot. String up to and including it can be dropped.
+                return possibleArrangements(condition.slice(firstQ + 1), damaged);
+            } else {
+                // Worth exploring.
+                const dot = condition.slice(firstQ + 1);
+                const hash = "#" + dot;
+                return possibleArrangements(dot, damaged) + possibleArrangements(hash, damaged);
+            }
         }
+
+        if (firstHash !== -1) {
+            const groupEndIdx = firstHash + sizeOfFirstGroup;
+            const groupChunk = condition.slice(firstHash, groupEndIdx);
+            const isWholeGroup = (groupEndIdx === condition.length) || (condition[groupEndIdx] !== "#");
+            const canMakeGroup = isWholeGroup && groupChunk.match(/^[?#]+$/);
+
+            if (canMakeGroup) {
+                return possibleArrangements(condition.slice(groupEndIdx + 1), restOfGroups);
+            }
+        }
+
+        // If we reach here, "damaged" can't describe this condition string.
+        return 0;
     }
 
-    // If we reach here, "damaged" can't describe this condition string.
-    return 0;
+    const result = wrapped();
+    cache.set(hashKey, result);
+    return result;
 }
 
 export async function solvePart2(lines: Sequence<string>) {
     const inputs = lines.map(unfold).map(parseLine);
-    const arrangements = inputs.map(x => possibleArrangements(...x));
+    const arrangements = inputs.map(([condition, damaged]) => possibleArrangements(condition, damaged));
     return Sequence.sum(arrangements);
 }
 
