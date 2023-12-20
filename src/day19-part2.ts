9946c54216daf91e38991ea8668efae1d3833b65
Neil Vass <neil.vass+github@gmail.com>
2023-12-20 00:06:40 +0000

    Part 2 coming along

---
+++ src/day19-part2.ts
@@ -0,0 +1,160 @@
+import {linesFromFile} from "./helpers.js";
+import {Sequence} from "./sequence.js";
+
+
+export type Rule = (partsRage: PartsRange) => { matched: {destination: string, range: PartsRange}, unmatched: PartsRange | null }
+
+export const ACCEPT = "A";
+export const REJECT = "R";
+export const PASS = "PASS";
+
+class CategoryRange {
+    readonly count;
+    constructor(readonly min: number, readonly max: number) {
+        this.count = max - min +1;
+    }
+
+    static readonly full = new CategoryRange(1, 4000);
+}
+
+export class PartsRange {
+    private constructor(private readonly categories: {[index: string]: CategoryRange}) {}
+
+    static readonly full = new PartsRange({ "x": CategoryRange.full,
+                                            "m": CategoryRange.full,
+                                            "a": CategoryRange.full,
+                                            "s": CategoryRange.full});
+
+    combinations() {
+        return Object.values(this.categories).reduce((acc,val) => acc * val.count, 1);
+    }
+
+    splitGreaterThan(category: string, value: number) {
+        const current = this.categories[category];
+        const matched = {...this.categories};
+        matched[category] = new CategoryRange(value+1, current.max);
+        const unmatched = {...this.categories};
+        unmatched[category] = new CategoryRange(current.min, value);
+        return [new PartsRange(matched), new PartsRange(unmatched)];
+    }
+
+    splitLessThan(category: string, value: number) {
+        const current = this.categories[category];
+        const matched = {...this.categories};
+        matched[category] = new CategoryRange(current.min, value-1);
+        const unmatched = {...this.categories};
+        unmatched[category] = new CategoryRange(value, current.max);
+        return [new PartsRange(matched), new PartsRange(unmatched)];
+    }
+}
+
+
+export class Workflow {
+    private readonly rules = new Array<Rule>();
+
+    constructor(public readonly name: string) {}
+
+    add(rule: Rule) {
+        this.rules.push(rule);
+    }
+
+    process(partsRange: PartsRange) {
+        const destinationsAndRanges = new Map<string, PartsRange>();
+        let remainingRange = partsRange;
+        for (const rule of this.rules) {
+            const result = rule(remainingRange);
+            destinationsAndRanges.set(result.matched.destination, result.matched.range);
+            if (result.unmatched === null) break;
+            remainingRange = result.unmatched;
+        }
+        return destinationsAndRanges;
+    }
+}
+
+export class System {
+    private readonly steps = new Map<string, Workflow>();
+
+    addWorkflow(workflowStr: string) {
+        const workflow = parseWorkflow(workflowStr);
+        this.steps.set(workflow.name, workflow);
+    }
+
+    countAcceptableCombinations() {
+        const partsRange = PartsRange.full;
+        return this.countCombos(partsRange);
+
+    }
+
+    private countCombos(partsRange: PartsRange, entryPoint="in") {
+        let count = 0;
+        const workflow = this.steps.get(entryPoint)!;
+        const destinationsAndRanges = workflow.process(partsRange);
+        for (const [destination, range] of destinationsAndRanges) {
+            switch(destination) {
+                case REJECT:
+                    continue;
+                case ACCEPT:
+                    count += range.combinations();
+                    break;
+                default:
+                    count += this.countCombos(range, destination);
+            }
+        }
+        return count;
+    }
+}
+
+export function parseRule(ruleStr: string): Rule {
+    const endRuleMatch = ruleStr.match(/^\w+$/);
+    if (endRuleMatch !== null) {
+        return (partsRange: PartsRange) => ({ matched: {destination: ruleStr, range: partsRange}, unmatched: null });
+    }
+
+    const matchObject = ruleStr.match(/^([xmas])([<>])(\d+):(\w+)$/);
+    if (matchObject === null) throw new Error(`Unexpected format: ${ruleStr}`);
+    const [, category, op, valueStr, destination] = matchObject;
+    const val = +valueStr;
+
+    function rule(partsRange: PartsRange) {
+        if (op === "<") {
+            const [matchedRange, unmatchedRange] = partsRange.splitLessThan(category, val);
+            return { matched: {destination, range: matchedRange }, unmatched: unmatchedRange };
+        } else {
+            const [matchedRange, unmatchedRange] = partsRange.splitGreaterThan(category, val);
+            return { matched: {destination, range: matchedRange }, unmatched: unmatchedRange };
+        }
+    }
+    return rule;
+}
+
+export function parseWorkflow(workflowStr: string) {
+    const matchObject = workflowStr.match(/^(\w+){(.+)}$/);
+    if (matchObject === null) throw new Error(`Unexpected format: ${workflowStr}`);
+    const [, name, rulesStr] = matchObject;
+
+    const workflow = new Workflow(name);
+    rulesStr.split(",").map(parseRule).forEach(rule => workflow.add(rule));
+    return workflow;
+}
+
+export function parsePart(partStr: string) {
+    const matchObject = partStr.match(/^{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}$/);
+    if (matchObject === null) throw new Error(`Unexpected format: ${partStr}`);
+    const [,x,m,a,s] = matchObject.map(Number);
+    return {x,m,a,s};
+}
+
+
+export async function solvePart2(lines: Sequence<string>) {
+    const system = new System();
+    for await (const line of lines) {
+        if (line === "") break;
+        system.addWorkflow(line);
+    }
+    return system.countAcceptableCombinations();
+}
+
+// If this script was invoked directly on the command line:
+if (`file://${process.argv[1]}` === import.meta.url) {
+    const lines = linesFromFile("./data/day19.txt");
+}
---
+++ test/day19-part2.test.ts
@@ -0,0 +1,64 @@
+import {expect, describe, it} from "vitest";
+import * as day19pt2 from "../src/day19-part2.js";
+import {Sequence} from "../src/sequence.js";
+import {PartsRange} from "../src/day19-part2.js";
+import * as day19 from "../src/day19.js";
+
+
+
+describe("Part 2", () => {
+    it("PartsRange splits", () => {
+        const range = PartsRange.full;
+        expect(range.combinations()).toBe(4000 * 4000 * 4000 * 4000);
+
+        const [matched, unmatched] = range.splitGreaterThan("x", 1000);
+        expect(matched.combinations()).toBe(3000 * 4000 * 4000 * 4000);
+        expect(unmatched.combinations()).toBe(1000 * 4000 * 4000 * 4000);
+    });
+
+    it("Solves reject-all base case", () => {
+        const system = new day19pt2.System();
+        system.addWorkflow("in{R}");
+        expect(system.countAcceptableCombinations()).toBe(0);
+    });
+
+    it("Solves accept-all base case", () => {
+        const system = new day19pt2.System();
+        system.addWorkflow("in{A}");
+        expect(system.countAcceptableCombinations()).toBe(4000 * 4000 * 4000 * 4000);
+    });
+
+    it("Filters", () => {
+        const system = new day19pt2.System();
+        system.addWorkflow("in{x>100:R,A}");
+        expect(system.countAcceptableCombinations()).toBe(100 * 4000 * 4000 * 4000);
+    });
+
+    it("Counts acceptable combinations for simple system", () => {
+        expect("this").toBe("a real test")
+    });
+
+    it("Solves example", async () => {
+        const lines = new Sequence([
+            "px{a<2006:qkq,m>2090:A,rfg}",
+            "pv{a>1716:R,A}",
+            "lnx{m>1548:A,A}",
+            "rfg{s<537:gd,x>2440:R,A}",
+            "qs{s>3448:A,lnx}",
+            "qkq{x<1416:A,crn}",
+            "crn{x>2662:A,R}",
+            "in{s<1351:px,qqz}",
+            "qqz{s>2770:qs,m<1801:hdj,R}",
+            "gd{a>3333:R,R}",
+            "hdj{m>838:A,pv}",
+            "",
+            "{x=787,m=2655,a=1222,s=2876}",
+            "{x=1679,m=44,a=2067,s=496}",
+            "{x=2036,m=264,a=79,s=2244}",
+            "{x=2461,m=1339,a=466,s=291}",
+            "{x=2127,m=1623,a=2188,s=1013}",
+        ]);
+
+        expect(await day19pt2.solvePart2(lines)).toBe(167409079868000);
+    });
+});
