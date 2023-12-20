441b172d2863cacc141ae755bdba72713c82a199
Neil Vass <neil.vass+github@gmail.com>
2023-12-20 22:25:39 +0000

    Day 20 part 1 done

--- src/day20.ts
+++ src/day20.ts
@@ -1,7 +1,6 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
 import {FifoQueue} from "./graphSearch.js";
-import {sign} from "crypto";
 
 export const LOW = "-low->";
 export const HIGH = "-high->";
@@ -13,7 +12,7 @@ export type ModuleName = string;
 export type Signal = { sender: ModuleName, receiver: ModuleName, pulse: Pulse };
 
 export class Module {
-    constructor(readonly name: ModuleName) {}
+    constructor(readonly name: ModuleName, readonly receivers: ModuleName[]) {}
 
     receive(sender: ModuleName, pulse: Pulse): Signal[] {
         return [];
@@ -21,8 +20,8 @@ export class Module {
 }
 
 export class Broadcaster extends Module {
-    constructor(readonly receivers: ModuleName[]) {
-        super("broadcaster");
+    constructor(receivers: ModuleName[]) {
+        super("broadcaster", receivers);
     }
 
     receive(sender: ModuleName, pulse: Pulse): Signal[] {
@@ -33,26 +32,19 @@ export class Broadcaster extends Module {
 export class FlipFlop extends Module {
     private isOn = false;
 
-    constructor(name: ModuleName, private readonly receiver: ModuleName) {
-        super(name);
-    }
-
     receive(sender: ModuleName, pulse: Pulse): Signal[] {
         if (pulse === HIGH) {
             return [];
         } else {
             const sendPulse = this.isOn ? LOW : HIGH;
             this.isOn = !this.isOn;
-            return [{ sender: this.name, receiver: this.receiver, pulse: sendPulse }];
+            return this.receivers.map(receiver => ({ sender: this.name, receiver, pulse: sendPulse }));
         }
     }
 }
 
 export class Conjunction extends Module {
     private readonly inputStates = new Map<ModuleName, Pulse>();
-    constructor(name: ModuleName, private readonly receiver: ModuleName) {
-        super(name);
-    }
 
     setInput(name: ModuleName) {
         this.inputStates.set(name, LOW);
@@ -62,27 +54,44 @@ export class Conjunction extends Module {
         this.inputStates.set(sender, pulse);
         const allInputsHigh = [...this.inputStates.values()].every(v => v === HIGH);
         const sendPulse = allInputsHigh ? LOW : HIGH;
-        return [{ sender: this.name, receiver: this.receiver, pulse: sendPulse }];
+        return this.receivers.map(receiver => ({ sender: this.name, receiver, pulse: sendPulse }));
     }
 }
 
 export class System {
     private pulseQueue = new FifoQueue<Signal>();
 
-    private constructor(private readonly modules: Map<string, Module>) {}
+    private constructor(private readonly modules: Map<string, Module>) {
+        this.wireUpInputs();
+    }
 
+    wireUpInputs() {
+        const conjunctions = [...this.modules.values()].filter(m => m instanceof Conjunction) as Conjunction[];
+        for (const conj of conjunctions) {
+            for (const sender of this.modules.values()) {
+                if (sender.receivers.includes(conj.name)) {
+                    conj.setInput(sender.name);
+                }
+            }
+        }
+    }
 
-    pushTheButton() {
+    pushTheButton(logging=false) {
         let [lowCount, highCount] = [0, 0];
         this.pulseQueue.push({ sender: "button", receiver: "broadcaster", pulse: LOW });
         while (!this.pulseQueue.isEmpty()) {
             const signal = this.pulseQueue.pull()!;
-            const receiver = this.modules.get(signal.receiver)!
-            const responses = receiver.receive(signal.sender, signal.pulse);
-            responses.forEach(r => this.pulseQueue.push(r));
             signal.pulse === LOW ? lowCount++ : highCount++;
+
+            if (logging) console.log(`${signal.sender} ${signal.pulse} ${signal.receiver}`);
+
+            const receiver = this.modules.get(signal.receiver)
+            if (receiver !== undefined) {
+                const responses = receiver.receive(signal.sender, signal.pulse);
+                responses.forEach(r => this.pulseQueue.push(r));
+            }
         }
-        return lowCount * highCount;
+        return {lowCount, highCount};
     }
 
     static async buildFromDescription(lines: Sequence<string>) {
@@ -102,20 +111,33 @@ export function parseModule(moduleDescription: string) {
         return new Broadcaster(receivers);
     }
 
-    const flipMatch = moduleDescription.match(/^%(\w+) -> (\w+)$/);
+    const flipMatch = moduleDescription.match(/^%(\w+) -> (.+)$/);
     if (flipMatch !== null) {
-        const [, name, receiver] = flipMatch;
-        return new FlipFlop(name, receiver);
+        const [, name, receiversStr] = flipMatch;
+        const receivers = receiversStr.split(", ");
+        return new FlipFlop(name, receivers);
     }
 
-    const conjMatch = moduleDescription.match(/^&(\w+) -> (\w+)$/);
+    const conjMatch = moduleDescription.match(/^&(\w+) -> (.+)$/);
     if (conjMatch === null) throw new Error(`Unrecognized format: ${moduleDescription}`);
-    const [, name, receiver] = conjMatch;
-    return new Conjunction(name, receiver);
+    const [, name, receiversStr] = conjMatch;
+    const receivers = receiversStr.split(", ");
+    return new Conjunction(name, receivers);
+}
 
+export async function solvePart1(lines: Sequence<string>) {
+    const system = await System.buildFromDescription(lines);
+    let [lowTotal, highTotal] = [0, 0];
+    for (let i=0; i<1000; i++) {
+        const result = system.pushTheButton();
+        lowTotal += result.lowCount;
+        highTotal += result.highCount;
+    }
+    return lowTotal * highTotal;
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
-    const filepath = "./data/day20.txt";
+    const lines = linesFromFile("./data/day20.txt");
+    console.log(await solvePart1(lines));
 }
--- test/day20.test.ts
+++ test/day20.test.ts
@@ -12,6 +12,40 @@ describe("Part 1", () => {
             "&inv -> a",
         ]);
         const system = await day20.System.buildFromDescription(lines);
-        expect(system.pushTheButton()).toBe(8 * 4);
+        expect(system.pushTheButton()).toStrictEqual({lowCount: 8, highCount: 4});
+    });
+
+    it("Solves first example", async () => {
+        const lines = new Sequence([
+            "broadcaster -> a, b, c",
+            "%a -> b",
+            "%b -> c",
+            "%c -> inv",
+            "&inv -> a",
+        ]);
+        expect(await day20.solvePart1(lines)).toBe(32000000);
+    });
+
+    it("Presses second example individual times", async () => {
+        const lines = new Sequence([
+            "broadcaster -> a",
+            "%a -> inv, con",
+            "&inv -> b",
+            "%b -> con",
+            "&con -> output",
+        ]);
+        const system = await day20.System.buildFromDescription(lines);
+        expect(system.pushTheButton(true)).toStrictEqual({lowCount: 4, highCount: 4});
+    });
+
+    it("Solves second example", async () => {
+        const lines = new Sequence([
+            "broadcaster -> a",
+            "%a -> inv, con",
+            "&inv -> b",
+            "%b -> con",
+            "&con -> output",
+        ]);
+        expect(await day20.solvePart1(lines)).toBe(11687500);
     });
 });
