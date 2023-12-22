b19b8cb3cdcc629a46cdc1d6e7011d4bf79f1134
Neil Vass <neil.vass+github@gmail.com>
2023-12-22 23:26:40 +0000

    Day 21 part 2 in progress...

--- src/day21.ts
+++ src/day21.ts
@@ -1,8 +1,9 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
+import fs from "fs";
 
+let methodCalls = 0;
 let cacheHits = 0;
-let cacheMiss = 0;
 // I've tried doing this using a decorator, to mimic the Python functools "@cache" decorator.
 // That's because:
 // 1. It's neater code (method can just do its work, with caching logic elsewhere)
@@ -15,6 +16,7 @@ function memoize(decoratedMethod: Function, context: ClassMethodDecoratorContext
 
     if (context.kind === "method") {
         return function (this: any, ...args: any[]) {
+            methodCalls++;
             if (!cacheCollection.has(this)) {
                 cacheCollection.set(this, new Map<string, any>());
             }
@@ -25,7 +27,6 @@ function memoize(decoratedMethod: Function, context: ClassMethodDecoratorContext
                 cacheHits++;
                 return cache.get(hashKey);
             } else {
-                cacheMiss++;
                 const result = decoratedMethod.apply(this, args);
                 cache.set(hashKey, result);
                 return result;
@@ -43,6 +44,8 @@ class BetterSet extends Set<number> {
 type Pos = { x: number, y: number };
 
 export class Garden {
+    private infiniteBounds = false;
+
     private constructor(readonly grid: string[][], readonly start: Pos) {}
 
     isInsideBounds(pos: Pos) {
@@ -51,6 +54,15 @@ export class Garden {
     }
 
     isPlot(pos: Pos) {
+        if (this.infiniteBounds) {
+            const modPos = {
+                x: pos.x % this.grid.length,
+                y: pos.y % this.grid[0].length
+            };
+
+            return this.grid.at(modPos.x)!.at(modPos.y) !== "#";
+        }
+
         return this.isInsideBounds(pos) && this.grid[pos.x][pos.y] !== "#";
     }
 
@@ -75,7 +87,7 @@ export class Garden {
 
     numPlotsReachable(steps: number) {
         const result = this.plotsReachable(this.start, steps);
-        console.log(`cache hits: ${cacheHits}, misses: ${cacheMiss}`)
+        console.log(`${steps}: method calls: ${methodCalls}, cache hits: ${cacheHits}`);
         return result.size;
     }
 
@@ -93,6 +105,12 @@ export class Garden {
 
         return new Garden(grid, start);
     }
+
+    static async withInfiniteBounds(lines: Sequence<string>) {
+        const garden = await Garden.buildFromDescription(lines);
+        garden.infiniteBounds = true;
+        return garden;
+    }
 }
 
 
@@ -101,8 +119,13 @@ export async function solvePart1(lines: Sequence<string>) {
     return garden.numPlotsReachable(64);
 }
 
+export async function solvePart2(lines: Sequence<string>, steps=26501365) {
+    const garden = await Garden.withInfiniteBounds(lines);
+    return garden.numPlotsReachable(steps);
+}
+
+
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
-    const lines = linesFromFile("./data/day21.txt");
-    console.log(await solvePart1(lines));
+
 }
--- test/day21.test.ts
+++ test/day21.test.ts
@@ -20,19 +20,46 @@ beforeEach(() => {
 });
 
 describe("Part 1", () => {
-    it("Moves one step", async () => {
+    it("Moves 1 step", async () => {
         const garden = await day21.Garden.buildFromDescription(exampleLines);
         expect(garden.start).toStrictEqual({x:5, y:5});
         expect(garden.numPlotsReachable(1)).toBe(2);
     });
 
-    it("Moves two steps", async () => {
+    it("Moves 2 steps", async () => {
         const garden = await day21.Garden.buildFromDescription(exampleLines);
         expect(garden.numPlotsReachable(2)).toBe(4);
     });
 
-    it("Moves six steps", async () => {
+    it("Moves 6 steps", async () => {
         const garden = await day21.Garden.buildFromDescription(exampleLines);
         expect(garden.numPlotsReachable(6)).toBe(16);
+    });
+});
+
+describe("Part 2: infinite bounds", () => {
+    it("Moves 6 steps in infinite garden", async () => {
+        const garden = await day21.Garden.withInfiniteBounds(exampleLines);
+        expect(garden.numPlotsReachable(6)).toBe(16);
+    });
+
+    it("Moves 10 steps in infinite garden", async () => {
+        const garden = await day21.Garden.withInfiniteBounds(exampleLines);
+        expect(garden.numPlotsReachable(10)).toBe(50);
+    });
+
+    it("Moves 50 steps in infinite garden", async () => {
+        const garden = await day21.Garden.withInfiniteBounds(exampleLines);
+        expect(garden.numPlotsReachable(50)).toBe(1594);
+    });
+
+    it("Moves 100 steps in infinite garden", async () => {
+        const garden = await day21.Garden.withInfiniteBounds(exampleLines);
+        expect(garden.numPlotsReachable(100)).toBe(6536);
+    });
+
+    it("Moves 500 steps in infinite garden", async () => {
+        const garden = await day21.Garden.withInfiniteBounds(exampleLines);
+        expect(garden.numPlotsReachable(500)).toBe(167004);
     });
 });
