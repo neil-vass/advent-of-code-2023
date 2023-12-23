6bda52380fa05b00588c30308c40a554e5efa07f
Neil Vass <neil.vass+github@gmail.com>
2023-12-23 09:06:18 +0000

    Placeholders

---
+++ test/day23.test.ts
@@ -0,0 +1,8 @@
+import {expect, describe, it} from "vitest";
+import * as day23 from "../src/day23.js";
+
+describe("#fn", () => {
+    it("runs", () => {
+        expect(day23.fn("")).toBe("a real test");
+    });
+});
