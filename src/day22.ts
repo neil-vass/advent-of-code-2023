73de5419712c8c6b173b0478e415375dd7a76e42
Neil Vass <neil.vass+github@gmail.com>
2024-01-06 14:29:19 +0000

    Day 22 part 1 done

--- src/day22.ts
+++ src/day22.ts
@@ -1,12 +1,127 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
 
-export function fn(filepath: string) {
-    return "Hello, World!";
+
+export type Vector = { x: number, y: number, z: number };
+export type hashedXY = string;
+
+const hash = (x: number, y: number) : hashedXY => `(${x},${y})`;
+const ascending = (left: number, right: number) => left - right;
+
+export class Brick {
+    readonly restingOn = new Set<Brick>();
+    readonly supporting = new Set<Brick>();
+    constructor(readonly from: Vector, readonly to: Vector) {}
+
+    xyPositions() : hashedXY[] {
+        if (this.from.x !== this.to.x) {
+            const [xMin, xMax] = [this.from.x, this.to.x].sort(ascending);
+            const result = new Array<string>();
+            for (let x=xMin; x<=xMax; x++) {
+                result.push(hash(x,this.from.y));
+            }
+            return result;
+        } else {
+            const [yMin, yMax] = [this.from.y, this.to.y].sort(ascending);
+            const result = new Array<string>();
+            for (let y=yMin; y<=yMax; y++) {
+                result.push(hash(this.from.x, y));
+            }
+            return result;
+        }
+    }
+
+    zHeightOfBase() {
+        return Math.min(this.from.z, this.to.z);
+    }
+
+    zHeightOfTop() {
+        return Math.max(this.from.z, this.to.z);
+    }
+
+    verticalSize() {
+        return Math.abs(this.from.z - this.to.z) + 1;
+    }
+
+    fallTo(supportHeight: number) {
+        const fallDistance = this.zHeightOfBase() - supportHeight - 1;
+        this.from.z -= fallDistance;
+        this.to.z -= fallDistance;
+    }
+
+    static buildFromDescription(line: string) {
+        const m = line.match(/^(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)$/);
+        if (m === null) throw new Error(`Unrecognized format: ${line}`);
+        const from = { x: +m[1], y: +m[2], z: +m[3] };
+        const to = { x: +m[4], y: +m[5], z: +m[6] };
+        return new Brick(from, to);
+    }
+}
+
+export class Stack {
+    readonly topBricks = new Map<hashedXY, Brick>();
+    readonly bricks = new Array<Brick>();
+
+    private constructor() {}
+
+    add(fallingBrick: Brick) {
+        let stoppedBy = new Array<Brick>();
+
+        for (const xy of fallingBrick.xyPositions()) {
+            const topBrickAtThisPos = this.topBricks.get(xy);
+
+            if (topBrickAtThisPos !== undefined) {
+                if (stoppedBy.length === 0 || stoppedBy[0].zHeightOfTop() < topBrickAtThisPos.zHeightOfTop()) {
+                    stoppedBy = [topBrickAtThisPos];
+                } else if (stoppedBy[0].zHeightOfTop() === topBrickAtThisPos.zHeightOfTop()) {
+                    stoppedBy.push(topBrickAtThisPos);
+                }
+            }
+        }
+
+        for (const xy of fallingBrick.xyPositions()) {
+            this.topBricks.set(xy, fallingBrick);
+        }
+
+        for (const supportBrick of stoppedBy) {
+            fallingBrick.restingOn.add(supportBrick);
+            supportBrick.supporting.add(fallingBrick);
+        }
+
+        const supportHeight = stoppedBy.length === 0 ? 0 : stoppedBy[0].zHeightOfTop();
+        fallingBrick.fallTo(supportHeight);
+        this.bricks.push(fallingBrick);
+    }
+
+    safeDisintegrationCount() {
+        let count = 0;
+        for (const brick of this.bricks) {
+            count++;
+            for (const supportedBrick of brick.supporting) {
+                if (supportedBrick.restingOn.size === 1) {
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+
+    static async buildFromDescription(lines: Sequence<string>) {
+        const stack = new Stack();
+        const fallingBricks = await lines.map(Brick.buildFromDescription).toArray();
+        fallingBricks.sort((a,b) => a.zHeightOfBase() - b.zHeightOfBase());
+        for (const brick of fallingBricks) {
+            stack.add(brick);
+        }
+        return stack;
+    }
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
-    const filepath = "./data/day22.txt";
-    console.log(fn(filepath));
+    const lines = linesFromFile("./data/day22.txt");
+    const stack = await Stack.buildFromDescription(lines);
+    console.log(stack.safeDisintegrationCount());
 }
--- test/day22.test.ts
+++ test/day22.test.ts
@@ -1,8 +1,84 @@
 import {expect, describe, it} from "vitest";
 import * as day22 from "../src/day22.js";
+import {Sequence} from "../src/sequence.js";
 
-describe("#fn", () => {
-    it("runs", () => {
-        expect(day22.fn("")).toBe("a real test");
+describe("Part 1", () => {
+    it("Understands what space bricks occupy", () => {
+        const brick = day22.Brick.buildFromDescription("1,0,1~1,2,1");
+        expect(brick.from).toStrictEqual({x:1, y:0, z:1});
+        expect(brick.to).toStrictEqual({x:1, y:2, z:1});
+        expect(brick.xyPositions()).toStrictEqual(["(1,0)","(1,1)","(1,2)"]);
+        expect(brick.zHeightOfBase()).toBe(1);
+        expect(brick.zHeightOfTop()).toBe(1);
+        expect(brick.verticalSize()).toBe(1);
+    });
+
+    it("Stacks 1 brick", async () => {
+        const lines = new Sequence([
+            "1,0,1~1,2,1"
+        ]);
+        const stack = await day22.Stack.buildFromDescription(lines);
+        expect([...stack.topBricks.keys()]).toStrictEqual(["(1,0)","(1,1)","(1,2)"]);
+    });
+
+    it("In a 1-brick stack, it's safe to disintegrate that brick", async () => {
+        const lines = new Sequence([
+            "1,0,1~1,2,1"
+        ]);
+        const stack = await day22.Stack.buildFromDescription(lines);
+        expect(stack.safeDisintegrationCount()).toBe(1);
+    });
+
+    it("Stacks 2 bricks, one holds up the other", async () => {
+        const lines = new Sequence([
+            "1,0,1~1,2,1",
+            "0,0,2~2,0,2",
+        ]);
+        const stack = await day22.Stack.buildFromDescription(lines);
+        expect([...stack.topBricks.keys()]).toStrictEqual([
+            "(1,0)","(1,1)","(1,2)","(0,0)","(2,0)"]);
+
+        const [brickA, brickB] = stack.bricks;
+        expect(brickA.zHeightOfTop()).toBe(1);
+        expect(brickA.restingOn.size).toBe(0);
+        expect(brickA.supporting.size).toBe(1);
+
+        expect(brickB.zHeightOfBase()).toBe(2);
+        expect(brickB.zHeightOfTop()).toBe(2);
+
+
+        expect(stack.safeDisintegrationCount()).toBe(1);
+    });
+
+    it("Stacks whole example", async () => {
+        const lines = new Sequence([
+            "1,0,1~1,2,1",
+            "0,0,2~2,0,2",
+            "0,2,3~2,2,3",
+            "0,0,4~0,2,4",
+            "2,0,5~2,2,5",
+            "0,1,6~2,1,6",
+            "1,1,8~1,1,9",
+        ]);
+        const stack = await day22.Stack.buildFromDescription(lines);
+        expect(stack.bricks.at(-1)!.zHeightOfTop()).toBe(6);
+        expect(stack.safeDisintegrationCount()).toBe(5);
+    });
+
+    it("Listing same bricks in different order gives same stack", async () => {
+        // Given example has bricks ordered by distance from ground; here
+        // they're rearranged so some closer-to-ground bricks are listed later.
+        const lines = new Sequence([
+            "0,2,3~2,2,3",
+            "2,0,5~2,2,5",
+            "0,1,6~2,1,6",
+            "1,1,8~1,1,9",
+            "1,0,1~1,2,1",
+            "0,0,2~2,0,2",
+            "0,0,4~0,2,4",
+        ]);
+        const stack = await day22.Stack.buildFromDescription(lines);
+        expect(stack.bricks.at(-1)!.zHeightOfTop()).toBe(6);
+        expect(stack.safeDisintegrationCount()).toBe(5);
     });
 });
